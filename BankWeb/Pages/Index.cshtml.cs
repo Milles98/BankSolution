30702679b29dea0ecaf886138e24f5845e15ef90
Mille Elfver <mille.elfver98@gmail.com>
2024-03-21 14:07:58 +0100

    added template

--- BankWeb/Pages/Index.cshtml.cs
+++ BankWeb/Pages/Index.cshtml.cs
@@ -9,17 +9,12 @@ namespace BankWeb.Pages
         private readonly BankAppData2Context _context = context;
 
         public int TotalCustomers { get; set; }
-        public Dictionary<string, int> CustomersPerCountry { get; set; }
         public decimal TotalBalance { get; set; }
         public int TotalAccounts { get; set; }
 
         public void OnGet()
         {
             TotalCustomers = _context.Customers.Count();
-            CustomersPerCountry = _context.Customers
-                .GroupBy(c => c.Country)
-                .ToDictionary(g => g.Key, g => g.Count());
-
             TotalBalance = _context.Accounts.Sum(a => a.Balance);
             TotalAccounts = _context.Accounts.Count();
         }
