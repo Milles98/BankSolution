2546cab5651aa930ab04e65b89a0a23157563b50
Mille Elfver <mille.elfver98@gmail.com>
2024-03-22 09:11:39 +0100

    updated search function, now in the slider

---
+++ BankWeb/Pages/SearchResults.cshtml.cs
@@ -0,0 +1,36 @@
+using BankWeb.Data;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankWeb.Pages
+{
+    public class SearchResultsModel : PageModel
+    {
+        private readonly BankAppData2Context _context;
+
+        public SearchResultsModel(BankAppData2Context context)
+        {
+            _context = context;
+        }
+
+        public List<CustomerAccountViewModel> Customers { get; set; }
+
+        public async Task OnGetAsync(string query)
+        {
+            Customers = await _context.Customers
+                .Where(c => c.Givenname.Contains(query) || c.Surname.Contains(query) || c.City.Contains(query))
+                .Select(c => new CustomerAccountViewModel
+                {
+                    CustomerId = c.CustomerId,
+                    Givenname = c.Givenname,
+                    Surname = c.Surname,
+                    Streetaddress = c.Streetaddress,
+                    City = c.City
+                })
+                .Take(50)
+                .ToListAsync();
+        }
+    }
+}
