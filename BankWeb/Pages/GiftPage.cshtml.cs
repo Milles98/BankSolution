f13f1283e0de64e669b1ebf60997491c94c0ddbc
Mille Elfver <mille.elfver98@gmail.com>
2024-03-25 13:02:50 +0100

    added gift page from country statistics

---
+++ BankWeb/Pages/GiftPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BankWeb.Pages
+{
+    public class GiftPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
