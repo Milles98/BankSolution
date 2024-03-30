8e69e219edd1392bc2180e544111bdb110e1e5c6
Mille Elfver <mille.elfver98@gmail.com>
2024-03-30 08:57:16 +0100

    updated searchlogic for customer to search for id, name or city

--- BankWeb/Pages/SearchResults.cshtml.cs
+++ BankWeb/Pages/SearchResults.cshtml.cs
@@ -17,11 +17,29 @@ namespace BankWeb.Pages
         }
 
         public List<CustomerAccountViewModel> Customers { get; set; }
+        public int CurrentPage { get; set; } = 1;
+        public int TotalPages { get; set; }
 
-        public async Task OnGetAsync(string query)
+        public async Task OnGetAsync(string query, int page = 1)
         {
-            Customers = await _context.Customers
-                .Where(c => c.Givenname.Contains(query) || c.Surname.Contains(query) || c.City.Contains(query))
+            query = query.ToLower();
+
+            int id;
+            bool isNumeric = int.TryParse(query, out id);
+
+            var customers = _context.Customers
+                .Where(c => (isNumeric && c.CustomerId == id) ||
+                c.Givenname.ToLower().Contains(query) ||
+                c.Surname.ToLower().Contains(query) ||
+                c.City.ToLower().Contains(query));
+
+            var totalCustomers = await customers.CountAsync();
+            TotalPages = (int)Math.Ceiling(totalCustomers / 50.0);
+
+            Customers = await customers
+                .OrderBy(c => c.CustomerId)
+                .Skip((page - 1) * 50)
+                .Take(50)
                 .Select(c => new CustomerAccountViewModel
                 {
                     CustomerId = c.CustomerId,
@@ -30,8 +48,12 @@ namespace BankWeb.Pages
                     Streetaddress = c.Streetaddress,
                     City = c.City
                 })
-                .Take(50)
                 .ToListAsync();
+
+            ViewData["TotalPages"] = TotalPages;
+            ViewData["CurrentPage"] = page;
         }
+
+
     }
 }
