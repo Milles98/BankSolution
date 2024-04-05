0a105e2ef59a89d51df2d7d78bc29158895a2f14
Mille Elfver <mille.elfver98@gmail.com>
2024-04-05 13:17:40 +0200

    Added new functionality for admin, cannot delete itself

--- BankWeb/Pages/UsersFolder/ManageUsers.cshtml.cs
+++ BankWeb/Pages/UsersFolder/ManageUsers.cshtml.cs
@@ -10,17 +10,22 @@ namespace BankWeb.Pages.UsersFolder
     public class ManageUsersModel : PageModel
     {
         private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
 
-        public ManageUsersModel(UserManager<IdentityUser> userManager)
+        public ManageUsersModel(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         public IList<IdentityUser> Users { get; set; }
+        public string LoggedInUserId { get; set; }
 
         public async Task OnGetAsync()
         {
             Users = await _userManager.Users.ToListAsync();
+            var loggedInUser = await _signInManager.UserManager.GetUserAsync(User);
+            LoggedInUserId = loggedInUser?.Id;
         }
 
         public async Task<IList<string>> GetRoles(IdentityUser user)
@@ -28,4 +33,5 @@ namespace BankWeb.Pages.UsersFolder
             return await _userManager.GetRolesAsync(user);
         }
     }
+
 }
