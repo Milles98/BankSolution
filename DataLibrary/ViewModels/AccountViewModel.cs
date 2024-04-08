5a748993d24144efbf233a7d4617c79b51cf48e9
Mille Elfver <mille.elfver98@gmail.com>
2024-04-08 10:32:44 +0200

    Added validation for frequency account creation

--- DataLibrary/ViewModels/AccountViewModel.cs
+++ DataLibrary/ViewModels/AccountViewModel.cs
@@ -7,8 +7,11 @@ namespace DataLibrary.ViewModels
     public class AccountViewModel
     {
         public string? AccountId { get; set; }
+
+        [Required(ErrorMessage = "Frequency is required.")]
         public string? Frequency { get; set; }
         public string? Created { get; set; }
+
         [Range(50, 50000, ErrorMessage = "Initial deposit must be between 50 and 50.000 SEK.")]
         public decimal Balance { get; set; }
         public string? Type { get; set; }
