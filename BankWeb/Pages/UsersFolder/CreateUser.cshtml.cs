288107b03f67df4ff3466cb55a5d88c5e6f12fbe
Mille Elfver <mille.elfver98@gmail.com>
2024-04-09 06:42:55 +0200

    Commented away unnessecary page for the moment

--- BankWeb/Pages/UsersFolder/CreateUser.cshtml.cs
+++ BankWeb/Pages/UsersFolder/CreateUser.cshtml.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Authorization;
+/*using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -32,3 +32,4 @@ public class CreateUserModel : PageModel
         return Page();
     }
 }
+*/
