bac1340a3addc4add59e93f6938b70dcc72140d4
Mille Elfver <mille.elfver98@gmail.com>
2024-04-10 08:46:51 +0200

    Updated customerVM and relevant mapping & methods to include all customer information for display

--- DataLibrary/MappingProfiles/CustomerProfile.cs
+++ DataLibrary/MappingProfiles/CustomerProfile.cs
@@ -16,6 +16,16 @@ public class CustomerProfile : Profile
                 Balance = d.Account.Balance,
                 Type = d.Type
             })))
+            .ForMember(dest => dest.Gender, opt => opt.MapFrom(src => src.Gender))
+            .ForMember(dest => dest.Streetaddress, opt => opt.MapFrom(src => src.Streetaddress))
+            .ForMember(dest => dest.City, opt => opt.MapFrom(src => src.City))
+            .ForMember(dest => dest.Zipcode, opt => opt.MapFrom(src => src.Zipcode))
+            .ForMember(dest => dest.Country, opt => opt.MapFrom(src => src.Country))
+            .ForMember(dest => dest.CountryCode, opt => opt.MapFrom(src => src.CountryCode))
+            .ForMember(dest => dest.Birthday, opt => opt.MapFrom(src => src.Birthday))
+            .ForMember(dest => dest.Telephonecountrycode, opt => opt.MapFrom(src => src.Telephonecountrycode))
+            .ForMember(dest => dest.Telephonenumber, opt => opt.MapFrom(src => src.Telephonenumber))
+            .ForMember(dest => dest.Emailaddress, opt => opt.MapFrom(src => src.Emailaddress))
             .ForMember(dest => dest.TotalBalance, opt => opt.MapFrom(src => src.Dispositions.Sum(d => d.Account.Balance)))
             .ForMember(dest => dest.NationalId, opt => opt.MapFrom(src => src.NationalId));
 
--- DataLibrary/ViewModels/CustomerAccountViewModel.cs
+++ DataLibrary/ViewModels/CustomerAccountViewModel.cs
@@ -3,13 +3,19 @@
     public class CustomerAccountViewModel
     {
         public int CustomerId { get; set; }
-        public string Givenname { get; set; }
+        public string Givenname { get; set; } = null!;
+        public string Surname { get; set; } = null!;
         public string Gender { get; set; } = null!;
-        public string Surname { get; set; }
-        public string NationalId { get; set; }
-        public string Streetaddress { get; set; }
-        public string City { get; set; }
-        public string Country { get; set; }
+        public string Streetaddress { get; set; } = null!;
+        public string City { get; set; } = null!;
+        public string Zipcode { get; set; } = null!;
+        public string Country { get; set; } = null!;
+        public string CountryCode { get; set; } = null!;
+        public DateOnly? Birthday { get; set; }
+        public string? Telephonecountrycode { get; set; }
+        public string? Telephonenumber { get; set; }
+        public string? Emailaddress { get; set; }
+        public string? NationalId { get; set; }
         public List<AccountViewModel> Accounts { get; set; }
         public decimal TotalBalance { get; set; }
     }
