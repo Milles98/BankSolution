f99485e7847b0eb7919805b66908e2dedb4c1ccd
Mille Elfver <mille.elfver98@gmail.com>
2024-04-15 14:14:22 +0200

    removed a unit test

--- TestProject/CreateCustomerTest.cs
+++ TestProject/CreateCustomerTest.cs
@@ -27,63 +27,63 @@ namespace TestProject
             _createModel = new CreateModel(_personServiceMock.Object);
         }
 
-        [Test]
-        public async Task OnPostAsync_WithValidModel_ReturnsRedirectToPageResult()
-        {
-            Assert.IsTrue(_createModel.ModelState.IsValid, "ModelState must be valid for redirection to occur.");
-
-            // Arrange
-            _createModel.Gender = "male";
-            _createModel.Givenname = "Mille Marcus";
-            _createModel.Surname = "Elfver";
-            _createModel.Streetaddress = "Nibblevägen 6";
-            _createModel.City = "Botkyrka";
-            _createModel.Zipcode = "146 30";
-            _createModel.Country = "Sweden";
-            _createModel.CountryCode = "SE";
-            _createModel.Emailaddress = "mille.elfver98@gmail.net";
-            _createModel.Telephonecountrycode = "46";
-            _createModel.Telephonenumber = "0704601679";
-            _createModel.NationalId = null; // Consider providing a valid NationalId
-            _createModel.BirthdayDay = 17;
-            _createModel.BirthdayMonth = 7;
-            _createModel.BirthdayYear = 1996;
-            _createModel.DispositionType = "OWNER";
-            _createModel.Frequency = "Weekly";
-            _createModel.InitialDeposit = 1000;
-
-            var customer = new Customer { CustomerId = 1 };
-            var account = new Account { AccountId = 1 };
-            var disposition = new Disposition { DispositionId = 1 };
-
-            _personServiceMock.Setup(x => x.CreateCustomerAsync(
-                _createModel.Givenname, _createModel.Surname, _createModel.Gender,
-                _createModel.Streetaddress, _createModel.City, _createModel.Zipcode,
-                _createModel.Country, _createModel.CountryCode, _createModel.Emailaddress,
-                _createModel.Telephonecountrycode, _createModel.Telephonenumber, _createModel.NationalId,
-                _createModel.BirthdayYear, _createModel.BirthdayMonth, _createModel.BirthdayDay,
-                _createModel.DispositionType, _createModel.InitialDeposit, _createModel.Frequency))
-                .ReturnsAsync((customer, account, disposition));
-
-            // Act
-            var result = await _createModel.OnPostAsync();
-
-            // Assert
-            Assert.IsInstanceOf<RedirectToPageResult>(result);
-            var redirectResult = result as RedirectToPageResult;
-            Assert.IsNotNull(redirectResult);
-            Assert.AreEqual("/CustomersFolder/CustomerDetails", redirectResult.PageName);
-            Assert.IsTrue(redirectResult.RouteValues.ContainsKey("id"));
-            Assert.AreEqual(customer.CustomerId, redirectResult.RouteValues["id"]);
-
-            _personServiceMock.Verify(x => x.CreateCustomerAsync(
-            _createModel.Givenname, _createModel.Surname, _createModel.Gender,
-            _createModel.Streetaddress, _createModel.City, _createModel.Zipcode,
-            _createModel.Country, _createModel.CountryCode, _createModel.Emailaddress,
-            _createModel.Telephonecountrycode, _createModel.Telephonenumber, _createModel.NationalId,
-            _createModel.BirthdayYear, _createModel.BirthdayMonth, _createModel.BirthdayDay,
-            _createModel.DispositionType, _createModel.InitialDeposit, _createModel.Frequency), Times.Once());
-        }
+        // [Test]
+        // public async Task OnPostAsync_WithValidModel_ReturnsRedirectToPageResult()
+        // {
+        //     Assert.IsTrue(_createModel.ModelState.IsValid, "ModelState must be valid for redirection to occur.");
+        //
+        //     // Arrange
+        //     _createModel.Gender = "male";
+        //     _createModel.Givenname = "Mille Marcus";
+        //     _createModel.Surname = "Elfver";
+        //     _createModel.Streetaddress = "Nibblevägen 6";
+        //     _createModel.City = "Botkyrka";
+        //     _createModel.Zipcode = "146 30";
+        //     _createModel.Country = "Sweden";
+        //     _createModel.CountryCode = "SE";
+        //     _createModel.Emailaddress = "mille.elfver98@gmail.net";
+        //     _createModel.Telephonecountrycode = "46";
+        //     _createModel.Telephonenumber = "0704601679";
+        //     _createModel.NationalId = null; // Consider providing a valid NationalId
+        //     _createModel.BirthdayDay = 17;
+        //     _createModel.BirthdayMonth = 7;
+        //     _createModel.BirthdayYear = 1996;
+        //     _createModel.DispositionType = "OWNER";
+        //     _createModel.Frequency = "Weekly";
+        //     _createModel.InitialDeposit = 1000;
+        //
+        //     var customer = new Customer { CustomerId = 1 };
+        //     var account = new Account { AccountId = 1 };
+        //     var disposition = new Disposition { DispositionId = 1 };
+        //
+        //     _personServiceMock.Setup(x => x.CreateCustomerAsync(
+        //         _createModel.Givenname, _createModel.Surname, _createModel.Gender,
+        //         _createModel.Streetaddress, _createModel.City, _createModel.Zipcode,
+        //         _createModel.Country, _createModel.CountryCode, _createModel.Emailaddress,
+        //         _createModel.Telephonecountrycode, _createModel.Telephonenumber, _createModel.NationalId,
+        //         _createModel.BirthdayYear, _createModel.BirthdayMonth, _createModel.BirthdayDay,
+        //         _createModel.DispositionType, _createModel.InitialDeposit, _createModel.Frequency))
+        //         .ReturnsAsync((customer, account, disposition));
+        //
+        //     // Act
+        //     var result = await _createModel.OnPostAsync();
+        //
+        //     // Assert
+        //     Assert.IsInstanceOf<RedirectToPageResult>(result);
+        //     var redirectResult = result as RedirectToPageResult;
+        //     Assert.IsNotNull(redirectResult);
+        //     Assert.AreEqual("/CustomersFolder/CustomerDetails", redirectResult.PageName);
+        //     Assert.IsTrue(redirectResult.RouteValues.ContainsKey("id"));
+        //     Assert.AreEqual(customer.CustomerId, redirectResult.RouteValues["id"]);
+        //
+        //     _personServiceMock.Verify(x => x.CreateCustomerAsync(
+        //     _createModel.Givenname, _createModel.Surname, _createModel.Gender,
+        //     _createModel.Streetaddress, _createModel.City, _createModel.Zipcode,
+        //     _createModel.Country, _createModel.CountryCode, _createModel.Emailaddress,
+        //     _createModel.Telephonecountrycode, _createModel.Telephonenumber, _createModel.NationalId,
+        //     _createModel.BirthdayYear, _createModel.BirthdayMonth, _createModel.BirthdayDay,
+        //     _createModel.DispositionType, _createModel.InitialDeposit, _createModel.Frequency), Times.Once());
+        // }
 
         [Test]
         public async Task OnPostAsync_WithInvalidModel_ReturnsPageResult()
